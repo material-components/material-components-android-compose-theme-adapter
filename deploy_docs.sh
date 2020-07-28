f26c5cdf6c1bdf5cc6f3f18a8e061008a1b501ec
Chris Banes <chrisbanes@google.com>
2020-07-28 10:32:41 +0100

    Docs and API files
    - We now track API files in each modules `api` directory.
    - We also build docs via mkdocs which are deploy to GitHub
    pages.

---
+++ deploy_docs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp README.md docs/index.md
+cp CONTRIBUTING.md docs/contributing.md
+
+sed -i 's/CONTRIBUTING.md/\/contributing/' docs/index.md
+sed -i 's/docs\/using-snapshot-version.md/using-snapshot-version/' docs/index.md
+
+# Deploy to Github pages.
+mkdocs gh-deploy
--- lib/src/main/java/com/google/android/material/composethemeadapter/MdcTheme.kt
+++ lib/src/main/java/com/google/android/material/composethemeadapter/MdcTheme.kt
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+@file:JvmName("MdcTheme")
 package com.google.android.material.composethemeadapter
 
 import android.content.Context
