e03c6afc1e73db82571c2497b322f7cb5af49bf8
Nick Rout <nickrout@google.com>
2021-02-11 09:36:40 +0100

    Update to activity-compose alpha02

--- buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
+++ buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
@@ -67,6 +67,10 @@ object Libs {
 
         const val coreKtx = "androidx.core:core-ktx:1.3.1"
 
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha02"
+        }
+
         object Test {
             private const val version = "1.3.0"
             const val runner = "androidx.test:runner:$version"
