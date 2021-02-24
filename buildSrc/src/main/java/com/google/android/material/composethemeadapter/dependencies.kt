ea17a27cc3eb3ee3add0886d1cff3e872bc22a45
Nick Rout <nickrout@google.com>
2021-02-24 17:43:10 +0100

    Update to Compose 1.0.0-beta01

--- buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
+++ buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha06"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha07"
 
     const val gradleMavenPublishPlugin = "com.vanniktech:gradle-maven-publish-plugin:0.12.0"
 
@@ -41,7 +41,7 @@ object Libs {
 
     object AndroidX {
         object Compose {
-            const val snapshot = "7154166"
+            const val snapshot = ""
 
             @JvmStatic
             val snapshotUrl: String
@@ -52,7 +52,7 @@ object Libs {
                     else -> throw IllegalArgumentException("Snapshot version not set")
                 }
 
-            const val version = "1.0.0-SNAPSHOT"
+            const val version = "1.0.0-beta01"
 
             const val runtime = "androidx.compose.runtime:runtime:$version"
             const val foundation = "androidx.compose.foundation:foundation:${version}"
@@ -68,7 +68,7 @@ object Libs {
         const val coreKtx = "androidx.core:core-ktx:1.3.1"
 
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.0-SNAPSHOT"
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha03"
         }
 
         object Test {
