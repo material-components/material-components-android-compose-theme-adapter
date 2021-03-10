2022f409bb633897c6068fda5dc76d93947ca3ba
Nick Rout <nickrout@google.com>
2021-03-10 09:58:47 +0100

    Add AppCompat dependency

--- buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
+++ buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
@@ -65,6 +65,8 @@ object Libs {
             const val test = "androidx.compose.ui:ui-test-junit4:${version}"
         }
 
+        const val appcompat = "androidx.appcompat:appcompat:1.3.0-beta01"
+
         const val coreKtx = "androidx.core:core-ktx:1.3.1"
 
         object Activity {
