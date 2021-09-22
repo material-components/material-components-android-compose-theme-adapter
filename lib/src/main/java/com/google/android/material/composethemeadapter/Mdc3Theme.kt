f3bc36258e438834a7e1997fda7dda1e9ce0c97a
Nick Rout <nickrout@google.com>
2021-09-22 10:35:35 +0200

    Apply spotless

--- lib/src/main/java/com/google/android/material/composethemeadapter/Mdc3Theme.kt
+++ lib/src/main/java/com/google/android/material/composethemeadapter/Mdc3Theme.kt
@@ -20,12 +20,12 @@ package com.google.android.material.composethemeadapter
 import android.content.Context
 import android.view.View
 import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
