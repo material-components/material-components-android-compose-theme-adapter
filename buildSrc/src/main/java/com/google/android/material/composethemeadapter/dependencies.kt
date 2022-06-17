3bcd5b7567ffd89148d2db1a35a9f21c1c9e8ea2
Nick Rout <nickrout@google.com>
2022-06-17 11:27:45 +0200

    Update to Core 1.8.0

--- buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
+++ buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
@@ -68,7 +68,7 @@ object Libs {
 
         const val appcompat = "androidx.appcompat:appcompat:1.4.1"
 
-        const val coreKtx = "androidx.core:core-ktx:1.8.0-rc02"
+        const val coreKtx = "androidx.core:core-ktx:1.8.0"
 
         object Activity {
             const val activityCompose = "androidx.activity:activity-compose:1.5.0-rc01"
