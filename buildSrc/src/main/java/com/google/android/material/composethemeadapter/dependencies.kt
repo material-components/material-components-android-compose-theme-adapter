3df483e9461aa4d1f1d4c1ca5ad305d061aa011d
Nick Rout <nickrout@google.com>
2022-07-28 10:53:55 +0200

    Update to Activity 1.5.1

--- buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
+++ buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
@@ -72,7 +72,7 @@ object Libs {
         const val coreKtx = "androidx.core:core-ktx:1.8.0"
 
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.5.0"
+            const val activityCompose = "androidx.activity:activity-compose:1.5.1"
         }
 
         object Test {
