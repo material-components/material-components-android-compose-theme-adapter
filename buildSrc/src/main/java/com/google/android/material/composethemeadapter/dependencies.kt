618b0138a3a42822cadc33f184b9eb4f1209c162
Nick Rout <nickrout@google.com>
2022-09-22 10:15:24 +0100

    Update to AGP 7.3.0 and Gradle 7.4

--- buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
+++ buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.2.2"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.3.0"
 
     const val gradleMavenPublishPlugin = "com.vanniktech:gradle-maven-publish-plugin:0.13.0"
 
