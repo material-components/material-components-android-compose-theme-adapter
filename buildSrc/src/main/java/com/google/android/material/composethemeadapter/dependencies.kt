a7a1eeb9e42766cebf92a5af08252d16e4869f2c
Nick Rout <nickrout@google.com>
2022-10-24 14:13:39 +0200

    Update to Compose Material 3 1.0.0

--- buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
+++ buildSrc/src/main/java/com/google/android/material/composethemeadapter/dependencies.kt
@@ -59,7 +59,7 @@ object Libs {
 
             const val ui = "androidx.compose.ui:ui:${version}"
             const val material = "androidx.compose.material:material:${version}"
-            const val material3 = "androidx.compose.material3:material3:1.0.0-rc01"
+            const val material3 = "androidx.compose.material3:material3:1.0.0"
 
             const val tooling = "androidx.compose.ui:ui-tooling:${version}"
             const val test = "androidx.compose.ui:ui-test-junit4:${version}"
